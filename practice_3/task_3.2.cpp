f2462f0e19c9a9662ad80e6f89795fe057995bb9
CNC5 <github@cnc5.dev>
2025-09-12 10:09:37 +0300

    [feature]: practice 3 task 2

---
+++ practice_3/task_3.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include "../common.cpp"
+
+int main() {
+    char buff[1];
+    std::ifstream fin("alphanum.txt");
+    print(fin.good());
+    while (fin.good()) {
+        fin.read(buff, 1);
+        if (std::isdigit(buff[0])) {
+            std::cout<<buff;
+        }
+    }
+    fin.close();
+    return 0;
+}
