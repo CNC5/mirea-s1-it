729840a133e55002d9c25c4d28fd2a6f326533b0
CNC5 <github@cnc5.dev>
2025-09-29 15:03:34 +0300

    [fix]: correct edge case

--- practice_1/task_1.2.cpp
+++ practice_1/task_1.2.cpp
@@ -6,7 +6,7 @@ int main(){
     cout<<"Введите числа a и b: ";
     cin>>a>>b;
     float sum=a+b, diff=a-b, prod=a*b;
-    if (b<1) {
+    if (b==0) {
         cout<<
             "Сумма: "<<sum<<
             ", разность: "<<diff<<
