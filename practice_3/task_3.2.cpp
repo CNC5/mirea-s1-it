642742478d8ca4a38a196a28909dfd4777cef6b7
CNC5 <github@cnc5.dev>
2025-10-01 02:21:29 +0300

    [fix]: write the file on disk in the code

--- practice_3/task_3.2.cpp
+++ practice_3/task_3.2.cpp
@@ -3,6 +3,13 @@
 #include "../common.cpp"
 
 int main() {
+    std::ofstream fout("alphanum.txt");
+    fout<<"5dkrqwd0335u9rb9w1nsmxm4"<<
+          "tx2xrz2g3422455a2nyltkck"<<
+          "jfp0cvypqqbo82hjzj0a67x1"<<
+          "1zi8j7yugkrf1jengw3og11tks4u"<<std::endl;
+    fout.close();
+
     char buff[1];
     std::ifstream fin("alphanum.txt");
     while (fin.good()) {
