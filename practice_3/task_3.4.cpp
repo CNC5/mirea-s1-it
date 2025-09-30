019a7eef694be199dd42aec36f6fa871f01f2c12
CNC5 <github@cnc5.dev>
2025-10-01 02:22:47 +0300

    [fix]: accept numbers as input and validate them

--- practice_3/task_3.4.cpp
+++ practice_3/task_3.4.cpp
@@ -31,8 +31,12 @@ int gcd_by_division(int a, int b) {
 
 int main() {
     int
-        a=48,
-        b=18;
+        a=input_int("Введите число a: "),
+        b=input_int("Введите число b: ");
+    if (a<=0 or b<=0) {
+        print("Оба числа должны быть положительными")
+        return 1;
+    }
     print("НОД вычитанием: "+str(gcd_by_subtraction(a, b)));
     print("НОД делением: "+str(gcd_by_division(a, b)));
     return 0;
