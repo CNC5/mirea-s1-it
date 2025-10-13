ae5bd3d42ec4a99e7ee8890bb3b4cf31b3e77252
CNC5 <github@cnc5.dev>
2025-10-13 10:50:48 +0300

    [fix]: unlimited read buffer

--- practice_3/task_3.1.cpp
+++ practice_3/task_3.1.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include "../common.cpp"
 
 int main() {
-    const int buffer_size=200;
     std::ofstream fout("task_3.1.txt");
     fout<<"Работа с файлами в C++"<<std::endl;
     fout.close();
 
-    char buff[buffer_size];
     std::ifstream fin("task_3.1.txt");
-    fin.read(buff, buffer_size);
+    std::string result;
+    while (getline(fin, result)) {
+        std::cout<<result;
+    }
     fin.close();
-    std::cout<<buff<<std::endl;
     return 0;
 }
